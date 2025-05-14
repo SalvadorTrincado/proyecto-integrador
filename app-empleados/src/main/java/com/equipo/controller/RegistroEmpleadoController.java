6258ab49d5727274744e4d961859690608aadac0
Jose David <josedavidalviares@gmail.com>
2025-05-14 12:59:37 +0200

    modificacion de los html, css y el controlador de RegistroEmpleadoController

--- app-empleados/src/main/java/com/equipo/controller/RegistroEmpleadoController.java
+++ app-empleados/src/main/java/com/equipo/controller/RegistroEmpleadoController.java
@@ -15,6 +15,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
@@ -31,84 +33,108 @@ public class RegistroEmpleadoController {
 
     // --- Paso 1 ---
     @GetMapping("/registro_empleado_paso1")
-    public String mostrarFormularioPaso1(Model model) {
-        model.addAttribute("registroEmpleadoPaso1DTO", new RegistroEmpleadoPaso1DTO());
+    public String mostrarFormularioPaso1(@ModelAttribute("registroEmpleadoPaso1DTO") RegistroEmpleadoPaso1DTO paso1,
+                                         Model model,
+                                         HttpSession sesion) {
+
+        RegistroEmpleadoPaso1DTO paso1DTO = (RegistroEmpleadoPaso1DTO) sesion.getAttribute("registroEmpleadoPaso1DTO");
+        if (paso1DTO != null) {
+            model.addAttribute("registroEmpleadoPaso1DTO", paso1DTO);
+        }
+
+        model.addAttribute("mostrarErrores", false);
         return "aplicacion_corporativa/registro_empleado_paso1";
     }
 
     @PostMapping("/registro_empleado_paso1-post")
-    public String procesarFormularioPaso1(@Valid @ModelAttribute("registroEmpleadoPaso1DTO") RegistroEmpleadoPaso1DTO paso1DTO,
+    public String procesarFormularioPaso1(@Valid @ModelAttribute("registroEmpleadoPaso1DTO") RegistroEmpleadoPaso1DTO paso1,
                                           BindingResult result,
                                           Model model,
                                           HttpSession session) throws IOException {
-        if (result.hasErrors()) {
+        if(result.hasErrors()) {
+            model.addAttribute("mostrarErrores", true);
             return "aplicacion_corporativa/registro_empleado_paso1";
         }
-        session.setAttribute("paso1", paso1DTO);
+
+        session.setAttribute("registroEmpleadoPaso1DTO", paso1);
+
         return "redirect:/registro_empleado_paso2"; // Redirigir al paso 2
     }
 
     // --- Paso 2 ---
     @GetMapping("/registro_empleado_paso2")
-    public String mostrarFormularioPaso2(Model model, HttpSession session) {
-        RegistroEmpleadoPaso1DTO paso1DTO = (RegistroEmpleadoPaso1DTO) session.getAttribute("paso1");
-        if (paso1DTO == null) {
-            return "redirect:/registro_empleado_paso1"; // Si no hay datos del paso 1, volver al inicio
+    public String mostrarFormularioPaso2(@ModelAttribute("registroEmpleadoPaso2DTO") RegistroEmpleadoPaso2DTO paso2,
+                                         Model model,
+                                         HttpSession session) {
+
+        RegistroEmpleadoPaso2DTO paso2DTO = (RegistroEmpleadoPaso2DTO) session.getAttribute("registroEmpleadoPaso2DTO");
+
+        if (paso2DTO != null) {
+            model.addAttribute("registroEmpleadoPaso2DTO", paso2DTO);
         }
-        model.addAttribute("registroEmpleadoPaso2DTO", new RegistroEmpleadoPaso2DTO());
+
+        model.addAttribute("mostrarErrores", false);
+
         return "aplicacion_corporativa/registro_empleado_paso2";
     }
 
     @PostMapping("/registro_empleado_paso2-post")
-    public String procesarFormularioPaso2(@Valid @ModelAttribute("registroEmpleadoPaso2DTO") RegistroEmpleadoPaso2DTO paso2DTO,
+    public String procesarFormularioPaso2(@Valid @ModelAttribute("registroEmpleadoPaso2DTO") RegistroEmpleadoPaso2DTO paso2,
                                           BindingResult result,
                                           Model model,
                                           HttpSession session) {
         if (result.hasErrors()) {
+            model.addAttribute("mostrarErrores", true);
             return "aplicacion_corporativa/registro_empleado_paso2";
         }
-        session.setAttribute("paso2", paso2DTO);
+        session.setAttribute("registroEmpleadoPaso2DTO", paso2);
         return "redirect:/registro_empleado_paso3"; // Redirigir al paso 3
     }
 
     // --- Paso 3 ---
     @GetMapping("/registro_empleado_paso3")
-    public String mostrarFormularioPaso3(Model model, HttpSession session) {
-        RegistroEmpleadoPaso1DTO paso1DTO = (RegistroEmpleadoPaso1DTO) session.getAttribute("paso1");
-        RegistroEmpleadoPaso2DTO paso2DTO = (RegistroEmpleadoPaso2DTO) session.getAttribute("paso2");
-        if (paso1DTO == null || paso2DTO == null) {
-            return "redirect:/registro_empleado_paso1"; // Si faltan datos, volver al inicio
+    public String mostrarFormularioPaso3(@ModelAttribute("registroEmpleadoPaso3DTO") RegistroEmpleadoPaso3DTO paso3,
+                                         Model model, HttpSession session) {
+
+        RegistroEmpleadoPaso3DTO paso3DTO = (RegistroEmpleadoPaso3DTO) session.getAttribute("registroEmpleadoPaso3DTO");
+
+        if (paso3DTO != null) {
+            model.addAttribute("registroEmpleadoPaso3DTO", paso3DTO);
         }
-        model.addAttribute("registroEmpleadoPaso3DTO", new RegistroEmpleadoPaso3DTO());
         List<String> especialidadesPosibles = List.of("Análisis de datos", "Administración de servidores", "Seguridad informática", "Desarrollo backend", "Desarrollo frontend", "Diseño UI/UX");
         model.addAttribute("especialidadesPosibles", especialidadesPosibles);
+
+        model.addAttribute("mostrarErrores", false);
+
         return "aplicacion_corporativa/registro_empleado_paso3";
     }
 
     @PostMapping("/registro_empleado_paso3-post")
-    public String procesarFormularioPaso3(@Valid @ModelAttribute("registroEmpleadoPaso3DTO") RegistroEmpleadoPaso3DTO paso3DTO,
+    public String procesarFormularioPaso3(@Valid @ModelAttribute("registroEmpleadoPaso3DTO") RegistroEmpleadoPaso3DTO paso3,
                                           BindingResult result,
                                           Model model,
                                           HttpSession session) {
         if (result.hasErrors()) {
             List<String> especialidadesPosibles = List.of("Análisis de datos", "Administración de servidores", "Seguridad informática", "Desarrollo backend", "Desarrollo frontend", "Diseño UI/UX");
             model.addAttribute("especialidadesPosibles", especialidadesPosibles);
+            model.addAttribute("mostrarErrores", true);
             return "aplicacion_corporativa/registro_empleado_paso3";
         }
-        session.setAttribute("paso3", paso3DTO);
+        session.setAttribute("registroEmpleadoPaso3DTO", paso3);
         return "redirect:/registro_empleado_paso4"; // Redirigir al paso 4
     }
 
     // --- Paso 4 ---
     @GetMapping("/registro_empleado_paso4")
-    public String mostrarFormularioPaso4(Model model, HttpSession session) {
-        RegistroEmpleadoPaso1DTO paso1DTO = (RegistroEmpleadoPaso1DTO) session.getAttribute("paso1");
-        RegistroEmpleadoPaso2DTO paso2DTO = (RegistroEmpleadoPaso2DTO) session.getAttribute("paso2");
-        RegistroEmpleadoPaso3DTO paso3DTO = (RegistroEmpleadoPaso3DTO) session.getAttribute("paso3");
-        if (paso1DTO == null || paso2DTO == null || paso3DTO == null) {
-            return "redirect:/registro_empleado_paso1"; // Si faltan datos, volver al inicio
+    public String mostrarFormularioPaso4(@ModelAttribute("registroEmpleadoPaso4DTO") RegistroEmpleadoPaso4DTO paso4,
+                                         Model model, HttpSession session) {
+
+        RegistroEmpleadoPaso4DTO paso4DTO = (RegistroEmpleadoPaso4DTO) session.getAttribute("registroEmpleadoPaso4DTO");
+
+        if (paso4DTO != null) {
+            model.addAttribute("registroEmpleadoPaso4DTO", paso4DTO);
         }
-        model.addAttribute("registroEmpleadoPaso4DTO", new RegistroEmpleadoPaso4DTO());
+        model.addAttribute("mostrarErrores", false);
         return "aplicacion_corporativa/registro_empleado_paso4";
     }
 
@@ -118,28 +144,58 @@ public class RegistroEmpleadoController {
                                           Model model,
                                           HttpSession session) {
         if (result.hasErrors()) {
+            model.addAttribute("mostrarErrores", true);
             return "aplicacion_corporativa/registro_empleado_paso4";
         }
 
-        // Recuperar todos los datos de la sesión
-        RegistroEmpleadoPaso1DTO paso1DTO = (RegistroEmpleadoPaso1DTO) session.getAttribute("paso1");
-        RegistroEmpleadoPaso2DTO paso2DTO = (RegistroEmpleadoPaso2DTO) session.getAttribute("paso2");
-        RegistroEmpleadoPaso3DTO paso3DTO = (RegistroEmpleadoPaso3DTO) session.getAttribute("paso3");
+        session.setAttribute("registroEmpleadoPaso4DTO", paso4DTO);
 
-        // Crear una nueva entidad Empleado y poblarla
-        Empleado empleado = crearEmpleadoDesdeSesion(paso1DTO, paso2DTO, paso3DTO, paso4DTO);
+        return "redirect:/resumen/exito";
+    }
 
-        // Guardar la entidad Empleado utilizando el servicio
+    @GetMapping("/resumen/exito")
+    public String mostrarPaginaExito(HttpSession session,
+                                     Model modelo) {
+
+        RegistroEmpleadoPaso1DTO paso1DTO = (RegistroEmpleadoPaso1DTO) session.getAttribute("registroEmpleadoPaso1DTO");
+        RegistroEmpleadoPaso2DTO paso2DTO = (RegistroEmpleadoPaso2DTO) session.getAttribute("registroEmpleadoPaso2DTO");
+        RegistroEmpleadoPaso3DTO paso3DTO = (RegistroEmpleadoPaso3DTO) session.getAttribute("registroEmpleadoPaso3DTO");
+        RegistroEmpleadoPaso4DTO paso4DTO = (RegistroEmpleadoPaso4DTO) session.getAttribute("registroEmpleadoPaso4DTO");
+
+        if(paso1DTO == null) {
+            paso1DTO = new RegistroEmpleadoPaso1DTO();
+        }
+
+        if(paso2DTO == null) {
+            paso2DTO = new RegistroEmpleadoPaso2DTO();
+        }
+
+        if(paso3DTO == null) {
+            paso3DTO = new RegistroEmpleadoPaso3DTO();
+        }
+
+        if(paso4DTO == null) {
+            paso4DTO = new RegistroEmpleadoPaso4DTO();
+        }
+
+        modelo.addAttribute("registroEmpleadoPaso1DTO", paso1DTO);
+        modelo.addAttribute("registroEmpleadoPaso2DTO", paso2DTO);
+        modelo.addAttribute("registroEmpleadoPaso3DTO", paso3DTO);
+        modelo.addAttribute("registroEmpleadoPaso4DTO", paso4DTO);
+
+        return "aplicacion_corporativa/registro_empleado_paso5";
+    }
+
+    @PostMapping("/resumen/exito-post")
+    public String mostrarPaginaExitoPost(HttpSession session) {
+
+        Empleado empleado = crearEmpleadoDesdeSesion((RegistroEmpleadoPaso1DTO) session.getAttribute("registroEmpleadoPaso1DTO"),
+                (RegistroEmpleadoPaso2DTO) session.getAttribute("registroEmpleadoPaso2DTO"),
+                (RegistroEmpleadoPaso3DTO) session.getAttribute("registroEmpleadoPaso3DTO"),
+                (RegistroEmpleadoPaso4DTO) session.getAttribute("registroEmpleadoPaso4DTO"));
         empleadoService.guardarEmpleado(empleado);
-
-        // Limpiar los datos de la sesión
-        session.removeAttribute("paso1");
-        session.removeAttribute("paso2");
-        session.removeAttribute("paso3");
-        session.removeAttribute("paso4");
-
-        // Redirigir a la página de éxito
-        return "redirect:/resumen";
+        session.invalidate();
+        return "redirect:/aplicacion_corporativa/area_personal";
     }
 
     private Empleado crearEmpleadoDesdeSesion(RegistroEmpleadoPaso1DTO paso1DTO,
@@ -184,10 +240,5 @@ public class RegistroEmpleadoController {
         empleado.setFechaIncorporacion(paso4DTO.getFechaIncorporacion());
 
         return empleado;
-    }
-
-    @GetMapping("/resumen")
-    public String mostrarPaginaExito() {
-        return "aplicacion_corporativa/registro_empleado_paso5";
     }
 }
